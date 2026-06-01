e959425a6f70e71d179edc3ebb6d246991e5a4d3
unknown <masukawavini@hotmail.com>
2026-06-01 10:24:27 -0300

    disparar primeiro deploy

--- next.config.mjs
+++ next.config.mjs
@@ -7,6 +7,7 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  basePath: "/site-preserv", // Adicione isto temporariamente para testar no link do GitHub
 };
 
 export default nextConfig;
