111a0892c0e90eae4ae8c106c73f421e7e9adef3
unknown <masukawavini@hotmail.com>
2026-06-01 14:23:58 -0300

    Adicionado favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -15,19 +15,9 @@ export const metadata: Metadata = {
   icons: {
     icon: [
       {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
+        url: "/favicon.ico",
       },
     ],
-    apple: "/apple-icon.png",
   },
 };
 
